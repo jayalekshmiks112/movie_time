13d6b3d6f0639d4d778280093edb0cfd7903026b
Ashsenior <ashutoshjunior@gmail.com>
2023-05-23 11:08:36 +0530

    first

---
+++ search/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+#from post.views import index, NewPost, PostDetail, Tags, like, favourite
+from . import views
+
+urlpatterns = [
+    # path('', views.index, name='index'),
+]
