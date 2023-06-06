5baafc26bef66510c7b6101785fbb69d59e3d2d1
Ashutosh Bisht <49332381+Ashsenior@users.noreply.github.com>
2023-06-07 00:37:26 +0530

    Revert "Search and explore functionality."

--- ig_prj/urls.py
+++ ig_prj/urls.py
@@ -27,8 +27,7 @@ urlpatterns = [
     path('users/', include('authy.urls')),
     path('', include('post.urls')),
     path('message/', include('directs.urls')),
-    path('', include('notification.urls')),
-    path('search/', include('search.urls')),
+    path('',include('notification.urls')),
     #path('savefolder/',include('savefolder.urls')),
     # profile
     path('<username>/', UserProfile, name='profile'),
